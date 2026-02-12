73662ba837b8d53b43e13c9d295562277fd2eba4
tjooris <tom.jooris@hotmail.fr>
2026-02-12 17:49:10 +0100

    starting the socket management

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,4 +1,23 @@
+#include <map>
+#include <iostream>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+
 int main()
 {
+	int								_host;
+	int								_port;
+	std::string						_server_name;
+	std::map<int, bool>				_is_socket_listening;
+	int								_epoll_fd;
+
+
+
+
 	return (0);
 }
