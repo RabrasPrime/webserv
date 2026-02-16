a3612947780f56ac28e4102637c468189b5f4d36
tjooris <tom.jooris@hotmail.fr>
2026-02-16 16:15:11 +0100

    adding the class client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -5,7 +5,8 @@
 #ifndef WEBSERV_CLIENT_HPP
 #define WEBSERV_CLIENT_HPP
 
-#include "server/Server.hpp"
+#include "Server.hpp"
+#include <string>
 
 class Server;
 
@@ -14,13 +15,27 @@ class Client
 	private:
 		int	_fd;
 		Server	*_server;
+
+		std::string	_read_buffer;
+		std::string	_write_buffer;
 	public:
+		Client();
 		Client(int fd, Server *server);
 		~Client();
 
 		int get_fd() const;
 		Server* get_server() const;
 
+		std::string& get_read_buffer();
+		const std::string& get_read_buffer() const;
+
+		std::string& get_write_buffer();
+		const std::string& get_write_buffer() const;
+
+		char& operator[](size_t index);
+		const char& operator[](size_t index) const;
+
+		bool is_valid() const;
 };
 
 
